5ca9bb8872b56602e06f9e00bb5616c2443ebade
DEVANG SHARMA <devshu25@gmail.com>
2023-06-18 04:46:44 -0400

    Added Code

---
+++ build/precache-manifest.8960cd6c6b0cab26d5f777231c7b7e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2081ef093a03a8e5ff4abdea7ff3358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beb313cd38e1a513f969",
+    "url": "/static/css/main.e24c6389.chunk.css"
+  },
+  {
+    "revision": "924872b8d9ffe8fda2d6",
+    "url": "/static/js/2.0a917c6a.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.0a917c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb313cd38e1a513f969",
+    "url": "/static/js/main.c4ff1f98.chunk.js"
+  },
+  {
+    "revision": "7dbe809d8ae504040e68",
+    "url": "/static/js/runtime-main.ebd86647.js"
+  }
+]);
